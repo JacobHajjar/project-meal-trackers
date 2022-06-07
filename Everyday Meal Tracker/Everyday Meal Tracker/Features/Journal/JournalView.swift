19b615555436270e1ea46a7c68ffc75f298ea22c
Rodolfo Hernandez <rodolfoh@csu.fullerton.edu>
2022-06-07 15:05:14 -0700

    Started Project

---
+++ Everyday Meal Tracker/Everyday Meal Tracker/Features/Journal/JournalView.swift
@@ -0,0 +1,20 @@
+//
+//  JournalView.swift
+//  Everyday Meal Tracker
+//
+//  Created by csuftitan on 6/6/22.
+//
+
+import SwiftUI
+
+struct JournalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JournalView_Previews: PreviewProvider {
+    static var previews: some View {
+        JournalView()
+    }
+}
---
+++ Everyday Meal Tracker/Everyday Meal Tracker/Features/Startup/Everyday_Meal_TrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  Everyday_Meal_TrackerApp.swift
+//  Everyday Meal Tracker
+//
+//  Created by csuftitan on 6/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Everyday_Meal_TrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            StartupView()
+                .environment(\.managedObjectContext,persistenceController.container.viewContext)
+        }
+    }
+}
